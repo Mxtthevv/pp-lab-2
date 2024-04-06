96c44af4b6fffb2b01ff3a525d1992127d29bdab
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-04-06 12:38:47 +0200

    zad1.java

--- zad1.java
+++ zad1.java
@@ -8,22 +8,29 @@ public class zad1 {
         System.out.println("Ile ocen chcesz dodać?");
         int iloscOcen = scan.nextInt();
 
-        int[] oceny = new int[iloscOcen]; // tworzenie talbicy o podanym rozmiarze
+        int[] oceny = new int[iloscOcen];   // tworzenie talbicy o podanym rozmiarze
 
         for (int i = 0; i < iloscOcen; i++) {   // petla na pobieranie ocen i zapisywanie do tablicy
             System.out.println("Podaj ocenę numer " + (i + 1) + ":");
             oceny[i] = scan.nextInt();
         }
 
-        scan.close();
+        System.out.println("Twoje oceny to:");   // wyswietlanie ocen z tablicy
+        for (int ocena : oceny) {
+            System.out.println(ocena + " ");
+        }
 
-        int suma = 0;
-        for (int ocena : oceny) { // obliczanie sumy ocen
+        int suma = 0;   // obliczanie sumy ocen
+        for (int ocena : oceny) { 
             suma += ocena;
         }
+
+        System.out.println("Suma twoich ocen to: " + suma);
 
         double srednia = (double) suma / iloscOcen; // obliczanie sredniej arytmetycznej
 
         System.out.println("Średnia arytmetyczna twoich ocen to: " + srednia);
+
+        scan.close();
     }
 }
