4f87bf76ed928e2179b80abe87ccd9ea69d646eb
Mateusz Dziubak <mateusz@MacBook-Pro-Mateusz.local>
2024-04-06 12:05:56 +0200

    zad1.java

--- zad1.java
+++ zad1.java
@@ -4,18 +4,25 @@ public class zad1 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj swoją ocene z matematyki:" + " ");
-        int matematyka = scan.nextInt();
 
-        System.out.println("Podaj swoją ocene z fizyki:" + " ");
-        int fizyka = scan.nextInt();
+        System.out.println("Ile ocen chcesz dodać?");
+        int iloscOcen = scan.nextInt();
 
-        System.out.println("Podaj swoją ocene z chemii:" + " ");
-        int chemia = scan.nextInt();
+        int[] oceny = new int[iloscOcen]; // tworzenie talbicy o podanym rozmiarze
+
+        for (int i = 0; i < iloscOcen; i++) {   // petla na pobieranie ocen i zapisywanie do tablicy
+            System.out.println("Podaj ocenę numer " + (i + 1) + ":");
+            oceny[i] = scan.nextInt();
+        }
 
         scan.close();
 
-        int srednia = (matematyka + fizyka + chemia)/3;
+        int suma = 0;
+        for (int ocena : oceny) { // obliczanie sumy ocen
+            suma += ocena;
+        }
+
+        double srednia = (double) suma / iloscOcen; // obliczanie sredniej arytmetycznej
 
         System.out.println("Średnia arytmetyczna twoich ocen to: " + srednia);
     }
